ac997144f013f77af3bd5dad198f0aa3532b9d10
lttlmw <vvys83@gmail.com>
2022-06-02 02:52:56 +0300

    add: added event state and event update

---
+++ WebApplication1/WebApplication1/EventState.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1;
+
+public enum EventState
+{
+    NotStarted = 0,
+    Started = 1,
+    Ended = 2
+}
---
+++ WebApplication1/WebApplication1/Services/EventUpdateService.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services;
+
+public class EventUpdateService
+{
+    private readonly DatabaseContext _context;
+
+    public EventUpdateService(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public object? UpdateEventBase(Guid eventId, EventState eventState)
+    {
+        // eventId - id матча/турнира(?)
+        // если eventState=1 запрещаем делать ставки на это событие
+        // если eventState=2 в betMatch/betTournament меняем betResult
+        return null;
+    }
+}
