ac2e59c89d780ce8332eb8af4ab95f7b3ab8bc4b
lttlmw <vvys83@gmail.com>
2022-06-26 01:30:41 +0600

    add: added method to get team list in event

---
+++ WebApplication1/WebApplication1/Services/TournamentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Services;
+
+public class TournamentService
+{
+    public List<Guid> TeamListInEvent(Guid eventId)
+    {
+        // возвращает List<Guid> команд в событии
+        var request = WebRequest.Create($"https://localhost:5001/api/tournament/{eventId}/teams");
+        request.Method = WebRequestMethods.Http.Get;
+        var response = request.GetResponse();
+        var responseStream = response.GetResponseStream();
+        using var readStream = new StreamReader(responseStream, Encoding.UTF8); 
+        var responseString = readStream.ReadToEnd();
+        var teamList = JsonConvert.DeserializeObject<List<Guid>>(responseString);
+        return teamList ?? new List<Guid>();
+    }
+}
