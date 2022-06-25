e16b3e1124a795e417bf33858eada5094e364883
Dmitriy Tikhomirov <uroborosq@ya.ru>
2022-06-26 00:36:28 +0300

    feat: added docker support

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -44,12 +44,11 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
+        
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
-        app.UseHttpsRedirection();
+        
+
         app.UseRouting();
         app.UseAuthorization();
         app.UseEndpoints(endpoints =>
