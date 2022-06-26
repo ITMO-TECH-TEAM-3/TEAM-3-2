b2b1f73e6f7d5dee61976f4269db26c468ae3041
lttlmw <vvys83@gmail.com>
2022-06-26 18:05:11 +0600

    add: added match model and method to get teams in match

---
+++ WebApplication1/WebApplication1/Models/Match.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models;
+
+public class Match
+{
+    
+    public Guid Id { get; private init; }
+    public Guid RestultId { get; private init; }
+    public int Round { get; private init; }
+    public DateTime StartDateTime { get; private init; }
+    public Guid Team1Id { get; private init; }
+    public Guid Team2Id { get; private init; }
+    
+    public Match(Guid id, Guid resultId, int round, DateTime startDateTime, Guid team1Id, Guid team2Id, Guid restultId)
+    {
+        Id = id;
+        RestultId = restultId;
+        Round = round;
+        StartDateTime = startDateTime;
+        Team1Id = team1Id;
+        Team2Id = team2Id;
+    }
+}
--- WebApplication1/WebApplication1/Services/TournamentService.cs
+++ WebApplication1/WebApplication1/Services/TournamentService.cs
@@ -1,14 +1,15 @@
 ﻿using System.Net;
 using System.Text;
 using Newtonsoft.Json;
+using WebApplication1.Models;
 
 namespace WebApplication1.Services;
 
 public class TournamentService
 {
-    public List<Guid> TeamListInEvent(Guid eventId)
+    public List<Guid> TeamListInTournament(Guid eventId)
     {
-        // возвращает List<Guid> команд в событии
+        // возвращает List<Guid> команд в турнире
         var request = WebRequest.Create($"https://localhost:5001/api/tournament/{eventId}/teams");
         request.Method = WebRequestMethods.Http.Get;
         var response = request.GetResponse();
@@ -17,5 +18,25 @@ public class TournamentService
         var responseString = readStream.ReadToEnd();
         var teamList = JsonConvert.DeserializeObject<List<Guid>>(responseString);
         return teamList ?? new List<Guid>();
+    }
+    
+    public List<Guid> TeamListInMatch(Guid eventId)
+    {
+        // возвращает List<Guid> команд в матче
+        var request = WebRequest.Create($"https://localhost:5001/api/match/{eventId}");
+        request.Method = WebRequestMethods.Http.Get;
+        var response = request.GetResponse();
+        var responseStream = response.GetResponseStream();
+        using var readStream = new StreamReader(responseStream, Encoding.UTF8); 
+        var responseString = readStream.ReadToEnd();
+        var match = JsonConvert.DeserializeObject<Match>(responseString);
+        var teamList = new List<Guid>();
+        if (match == null)
+        {
+            return teamList;
+        }
+        teamList.Add(match.Team1Id);
+        teamList.Add(match.Team2Id);
+        return teamList;
     }
 }
