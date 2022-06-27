eea45e5403b48a4c991db116c1b05f90fb3aa858
Dmitriy Tikhomirov <uroborosq@ya.ru>
2022-06-27 10:50:09 +0300

    fix: datetime is nullable now

--- WebApplication1/WebApplication1/Models/Match.cs
+++ WebApplication1/WebApplication1/Models/Match.cs
@@ -4,13 +4,13 @@ public class Match
 {
     
     public Guid Id { get; private init; }
-    public Guid ResultId { get; private init; }
+    public Guid? ResultId { get; private init; }
     public int Round { get; private init; }
-    public DateTime StartDateTime { get; private init; }
+    public DateTime? StartDateTime { get; private init; }
     public Guid Team1Id { get; private init; }
     public Guid Team2Id { get; private init; }
     
-    public Match(Guid id, Guid resultId, int round, DateTime startDateTime, Guid team1Id, Guid team2Id)
+    public Match(Guid id, Guid resultId, int round, DateTime? startDateTime, Guid team1Id, Guid team2Id)
     {
         Id = id;
         ResultId = resultId;
